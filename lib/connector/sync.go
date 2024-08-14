773572b61774fbd73427280cfd0702c1bc393adb
Hannes Hansen <mail@hannesh.de>
2024-08-14 09:17:13 +0200

    only log errors in sync loop

--- lib/connector/sync.go
+++ lib/connector/sync.go
@@ -2,24 +2,28 @@ package connector
 
 import (
 	"context"
+	"fmt"
 	"time"
+
+	"github.com/SENERGY-Platform/analytics-fog-connector/lib/logging"
 )
 
-func (connector *Connector) RequestSyncs(doneCtx context.Context, syncIntervalInSeconds time.Duration) error {
+func (connector *Connector) RequestSyncs(doneCtx context.Context, syncIntervalInSeconds time.Duration) {
 	ticker := time.NewTicker(syncIntervalInSeconds * time.Second)
 
 	for {
 		select {
 		case <-doneCtx.Done():
 			ticker.Stop()
-			return nil
 		case <-ticker.C:
 			err := connector.RequestOperatorSync()
 			if err != nil {
-				return err 
+				logging.Logger.Error(fmt.Sprintf("Cant request operator sync %s", err.Error())) 
 			}
 			err = connector.RequestUpstreamSync()
-			return err
+			if err != nil {
+				logging.Logger.Error(fmt.Sprintf("Cant request upstream sync %s", err.Error())) 
+			}
 		}
 	}
 
